d38ed91c3b0ea5f27d3f04850b32155c980099f2
Pares <alfariz2004@gmail.com>
2025-03-27 23:01:16 +0700

    first thread

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+
+    use ::std::thread;
+    use ::std::time::Duration;
+
+    #[test]
+    fn test_create_thread() {
+        thread::spawn(|| {
+            for i in 1..=5 {
+                println!("Counter : {}", i);
+                thread::sleep(Duration::from_secs(1));
+            }
+        });
+
+        println!("Application finish");
+        thread::sleep(Duration::from_secs(7));
+    }
+
+}
